5a3c0952419546fcce902a53c85cb21e6a9f787b
fsikali <flemmingsikali499@gmail.com>
2026-03-09 14:13:34 +0300

    feat update

--- src/dt.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: "development" | "production" | "test";
-    PORT?: string;
-    FRONTEND_URL?: string;
-    DATABASE_URL: string;
-    JWT_SECRET?: string;
-  }
-}
