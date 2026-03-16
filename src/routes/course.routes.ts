e186753da193140be77d0ac2e65473865c778980
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-16 12:24:39 +0300

    Update course.routes.ts

---
+++ src/routes/course.routes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import auth from "../middleware/auth";
+import { permit } from "../middleware/roles";
+import * as courseCtrl from "../controllers/courseController";
+
+const router = express.Router();
+
+// Create Course
+router.post("/", auth, permit("teacher", "admin"), courseCtrl.createCourse);
+
+// Create Cohort
+router.post(
+  "/cohort",
+  auth,
+  permit("teacher", "admin"),
+  courseCtrl.createCohort
+);
+
+export default router;
